fb2e25915195e98ce5a43f73c2c668825446eb7f
abhishekh912 <abhishekhlamichhane@gmail.com>
2024-08-09 11:32:31 +0545

    finally solved

--- 469A.py
+++ 469A.py
@@ -3,16 +3,19 @@ def passornot():
     if not 1<=n<=100:
         exit()
     
-    x_levels=[int(num) for num in input().split()]
+    x_levels=list(map(int,input().split()))
     for x in x_levels:
         if not 0<=x<=n:
             exit()
-    y_levels=[int(num) for num in input().split()]
+    player1=x_levels[1:]
+    y_levels=list(map(int,input().split()))
     for y in y_levels:
         if not 0<=y<=n:
             exit()
-    merge_list=list(set(x_levels+y_levels))
+    player2=y_levels[1:]
+    merge_list=list(set(player1+player2))
     total_levels=list(range(1,n+1))
+
     if merge_list== total_levels:
         print("I become the guy.")
     else:
