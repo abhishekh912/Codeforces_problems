9e21deaa7c68efd80957b9dc1ab1746891127a61
abhishekh912 <abhishekhlamichhane@gmail.com>
2024-08-28 17:53:29 +0545

    george and accomodation

---
+++ 467A_cf.py
@@ -0,0 +1,10 @@
+def rooms():
+    n=int(input())
+    count=0
+    for i in range(n):
+        p,q=0,0
+        p,q=map(int,input().split())
+        if (q-p)>=2:
+            count+=1
+    print(count)
+rooms()
